edcd00f08d39d2bac8a447373955e3aea253f392
Dan Stronge <dnlstronge@gmail.com>
2023-03-31 15:04:08 +0100

    [x] - format & tidy

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,36 +1,32 @@
-import { useState, Component } from 'react';
-import User from './User';
+import { Component } from "react";
+import User from "./User";
 
-import classes from './Users.module.css';
-
-
+import classes from "./Users.module.css";
 
 // class based components use concstructor to define state
 // and state is always an object
 const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
+  { id: "u1", name: "Max" },
+  { id: "u2", name: "Manuel" },
+  { id: "u3", name: "Julie" },
 ];
 
 class Users extends Component {
-
   constructor() {
-    super()
+    super();
     this.state = {
       showUsers: true,
-      moreState: "test", // in the handler below only showUsers would change, React merges the state object 
-      usersList: DUMMY_USERS
-    }
+      moreState: "test", // in the handler below only showUsers would change, React merges the state object
+      usersList: DUMMY_USERS,
+    };
   }
   toggleUsersHandler() {
     this.setState((curstate) => {
-      return {showUsers: !curstate.showUsers}
-    })    // always takes an object) alwatys merges
+      return { showUsers: !curstate.showUsers };
+    }); // always takes an object) alwatys merges
   }
 
   render() {
-
     const usersList = (
       <ul>
         {DUMMY_USERS.map((user) => (
@@ -39,11 +35,10 @@ class Users extends Component {
       </ul>
     );
 
-
     return (
       <div className={classes.users}>
         <button onClick={this.toggleUsersHandler.bind(this)}>
-          {this.state.showUsers ? 'Hide' : 'Show'} Users
+          {this.state.showUsers ? "Hide" : "Show"} Users
         </button>
         {this.state.showUsers && usersList}
       </div>
@@ -51,16 +46,12 @@ class Users extends Component {
   }
 }
 
-
-
 // const Users = () => {
-  // const [showUsers, setShowUsers] = useState(true);
+// const [showUsers, setShowUsers] = useState(true);
 
-  // const toggleUsersHandler = () => {
-  //   setShowUsers((curState) => !curState);
-  // };
-
-
+// const toggleUsersHandler = () => {
+//   setShowUsers((curState) => !curState);
+// };
 
 //   return (
 //     <div className={classes.users}>
