215f57ca5d8f7f5ded287b0f1d1e19815bd0fd12
Dan Stronge <dnlstronge@gmail.com>
2023-03-31 13:31:07 +0100

    [x] - continue to implement CB comp

--- src/components/Users.js
+++ src/components/Users.js
@@ -3,39 +3,42 @@ import User from './User';
 
 import classes from './Users.module.css';
 
-const [showUsers, setShowUsers] = useState(true);
+
 
 // class based components use concstructor to define state
 // and state is always an object
-
-
-class Users extends Component {
-  constructor() {
-    this.state = {
-
-    }
-  }
-  toggleUsersHandler() {
-    setShowUsers((curState) => !curState);
-  }
-
-  render() {
-    return (
-      <div className={classes.users}>
-        <button onClick={toggleUsersHandler}>
-          {showUsers ? 'Hide' : 'Show'} Users
-        </button>
-        {showUsers && usersList}
-      </div>
-    );
-  }
-}
-
 const DUMMY_USERS = [
   { id: 'u1', name: 'Max' },
   { id: 'u2', name: 'Manuel' },
   { id: 'u3', name: 'Julie' },
 ];
+
+class Users extends Component {
+
+  constructor() {
+    this.state = {
+      showUsers: true,
+      moreState: "test",
+      usersList: DUMMY_USERS
+    }
+  }
+  toggleUsersHandler() {
+    this.setState({}) // always takes an object)
+  }
+
+  render() {
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler}>
+          {this.showUsers ? 'Hide' : 'Show'} Users
+        </button>
+        {this.showUsers && this.usersList}
+      </div>
+    );
+  }
+}
+
+
 
 // const Users = () => {
   // const [showUsers, setShowUsers] = useState(true);
